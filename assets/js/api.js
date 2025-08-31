84bac085c656d9c45a101f6248990241ce03fae0
ThallyFarias <thallyprofissional@hotmail.com>
2025-08-31 13:22:12 -0300

    adicionando readme

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '';
+    const url = 'https://raw.githubusercontent.com/ThallyFarias/portifolio-DIO/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
